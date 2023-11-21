d5bdf16cd6504959370e9d27d9cc5dbb1818d193
W0k3rKK <570488980@qq.com>
2023-11-21 09:44:20 +0800

    add request

--- src/main/java/com/linktochatgpt/service/Completions.java
+++ src/main/java/com/linktochatgpt/service/Completions.java
@@ -1,8 +1,16 @@
 package com.linktochatgpt.service;
 
+import com.linktochatgpt.bean.CompletionRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Completions {
 
+	@Autowired
+	private CompletionRequest request;
+
+	public void completions() {
+		request.setPrompt("默认测试");
+	}
 }
